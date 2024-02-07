11cc57229921c34041976dfcc84d296093c21af6
Alvin Mwakai <mwakaimwambala@gmail.com>
2024-02-08 00:41:15 +0300

    initialize and call UI

--- CalculatorApplication/src/Main.java
+++ CalculatorApplication/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            Ui uiCal = new Ui();
+            uiCal.init();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
